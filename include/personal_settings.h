7e7ba753317d1ed2ecfd8c3ff8d84b746593711c
acat <j.koeppeler@tu-berlin.de>
2021-04-16 22:42:03 +0200

    removed gitignore and personal_settings.h

--- include/personal_settings.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by acat on 16.04.21.
-//
-
-#ifndef ALGENG_WCE_SOLVER_PERSONAL_SETTINGS_H
-#define ALGENG_WCE_SOLVER_PERSONAL_SETTINGS_H
-#define PATH_TO_TEST_DATA "put your path to the test data folder here!!!(/include/personal_settings.h)"
-#endif //ALGENG_WCE_SOLVER_PERSONAL_SETTINGS_H
