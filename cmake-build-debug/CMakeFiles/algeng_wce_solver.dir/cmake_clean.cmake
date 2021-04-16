ea77d9f92d61220f4b1bcd093642a4af9cdbd266
acat <j.koeppeler@tu-berlin.de>
2021-04-16 11:21:07 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/algeng_wce_solver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algeng_wce_solver.dir/main.cpp.o"
+  "algeng_wce_solver"
+  "algeng_wce_solver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algeng_wce_solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/algeng_wce_solver.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for algeng_wce_solver.
+# This may be replaced when dependencies are built.
