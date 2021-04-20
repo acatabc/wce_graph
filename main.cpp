0921b758ec07f04b02f3545c08a61c51c2ac0c64
Julia Henkel <henkel@campus.tu-berlin.de>
2021-04-20 18:15:57 +0200

    remove couts

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 #include "include/utils.h"
 int main() {
 
-    WCE_Graph *g = parse_and_build_graph("../test_data/test_data.txt");
+    WCE_Graph *g = parse_and_build_graph("../test_data/a001.dimacs");
     Solver *s = new Solver(g);
-    g->printGraph(std::cout);
+
 
 #ifdef DEBUG
+    g->printGraph(std::cout);
     s->print_all_p3();
 #endif
     s->solve();
