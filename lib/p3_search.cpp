b4d3b39f6baf8ca9f034f7ecc8ab60e829cc47b2
acat <j.koeppeler@tu-berlin.de>
2021-05-23 17:10:39 +0200

    p3 struct initialization

--- lib/p3_search.cpp
+++ lib/p3_search.cpp
@@ -1,4 +1,6 @@
 
+#include <tuple>
+#include <algorithm>
 #include "Solver.h"
 #include "../include/utils.h"
 #define HEURISTIC 0
@@ -224,7 +226,7 @@ std::vector<Solver::p3> Solver::find_all_p3(){
                             abs(weight_j_k) < min_cost)
                             min_cost = abs(weight_j_k);
 
-                        Solver::p3 newP3 = {.i = i, .j = j, .k = k, .min_cost = min_cost, .cost_sum = cost_sum};
+                        Solver::p3 newP3 = {.i = i, .j = j, .k = k,  .cost_sum = cost_sum,.min_cost = min_cost};
                         allP3.push_back(newP3);
                     }
                 }
