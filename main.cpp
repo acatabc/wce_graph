b248e570575461d66087f260d4bd1cf4a8078521
acat <j.koeppeler@tu-berlin.de>
2021-05-24 13:04:50 +0200

    deleted dead code

--- main.cpp
+++ main.cpp
@@ -1,20 +1,9 @@
 
 
 #include "lib/Solver.h"
-#include "lib/Solver2.h"
 
 int main() {
-
-
-//    WCE_Graph *g = parse_and_build_graph("../test_data/a001.dimacs");
-//    Solver2 *s = new Solver2();
     Solver *s = new Solver();
-
-
-//    g->printGraph(std::cout);
-//    s->dataRed_remove_existing_clique();
-
     s->solve();
-
     return 0;
 }
